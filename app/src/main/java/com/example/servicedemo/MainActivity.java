156327e6cd3486c1fb937ba2bbc94d17654fc2fb
burningman2k20 <burningman2k9@gmail.com>
2020-08-12 15:42:55 -0400

    initial commit

---
+++ app/src/main/java/com/example/servicedemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.servicedemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    // Start the service
+    public void startService(View view) {
+        startService(new Intent(this, MyService.class));
+    }
+    // Stop the service
+    public void stopService(View view) {
+        stopService(new Intent(this, MyService.class));
+    }
+}
